bd2736e2e721315b111d243ce3152609681d8ed4
AECunningham <aecunningham@aecmail.plus.com>
2015-12-31 10:03:40 -0800

    Problem 2

---
+++ Problem2Partg.sql
@@ -0,0 +1,7 @@
+select value from 
+(select A.row_num as row_num,B.col_num as col_num,sum(A.value*B.value) as value
+from A,B
+where A.col_num = B.row_num
+group by A.row_num,B.col_num
+having sum(A.value*B.value) > 0) x
+where row_num = 2 and col_num = 3;
